b2df43b8840c345cbb66220ad06eca269db60a7c
Mathieu Clément <mcfly1893@gmail.com>
2022-08-30 19:05:58 +0200

    Add more DB methods

--- src/art_exhibition_api/db.py
+++ src/art_exhibition_api/db.py
@@ -1,5 +1,6 @@
-from flask import Flask, current_app, g
+from flask import Flask, current_app, g, json
 from pymongo.collection import Collection
+from bson.objectid import ObjectId
 from flask_pymongo import PyMongo
 
 def get_mongo() -> PyMongo:
@@ -9,9 +10,16 @@ def get_mongo() -> PyMongo:
     return g.mongo
 
 def get_exhibitions() -> Collection:
-    get_mongo().db.exhibitions
+    return get_mongo().db.exhibitions
 
-def close_mongo():
+def all_exhibitions():
+    return list(get_exhibitions().find({}))
+
+def replace_exhibitions(exhibitions):
+    get_exhibitions().drop()
+    get_exhibitions().insert_many(exhibitions)
+
+def close_mongo(e=None):
     mongo: PyMongo = g.pop('mongo', None)
 
     if mongo is not None:
@@ -19,3 +27,10 @@ def close_mongo():
 
 def init_app(app: Flask):
     app.teardown_appcontext(close_mongo)
+    app.json_encoder = JSONEncoder
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
